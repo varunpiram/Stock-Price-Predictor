3b71cf4640fb538e62cbb8648f214a7a2043c93c
varunpiram <varunpiram@gmail.com>
2023-08-21 03:10:38 -0700

    Added economic data - Data class complete(?)

--- data.py
+++ data.py
@@ -9,11 +9,16 @@ from yfinance import Ticker
 
 
 
-class sentimentScorer():
+class dataGenerator():
 
     def __init__(self):
         self.tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
         self.model = DistilBertModel.from_pretrained('distilbert-base-uncased')
+
+    def read_economic_data(self):
+        df_economic = pd.read_csv('WorldEconomicData.csv')
+        df_economic['Date'] = pd.to_datetime(df_economic['Date'], format='%d-%b-%y')
+        return df_economic
         
     def compute_score(self,row, ticker):
         print(f"Processing date: {row['Date']}")  # Debugging statement
@@ -81,7 +86,7 @@ class sentimentScorer():
         columns_fundamental = ['Trailing PE', 'Forward PE', 'Dividend Yield']
         
         # Existing Columns: Price Data
-        columns_price = ['Open', 'High', 'Low', 'Close', 'Sentiment_Score']
+        columns_price = ['Open', 'High', 'Low', 'Close', 'Sentiment Score']
 
         # Handle Dividend Yield by setting NaN values to 0
         df['Dividend Yield'].fillna(0, inplace=True)
@@ -98,72 +103,84 @@ class sentimentScorer():
         
     def score(self, ticker):
         df = pd.read_csv('WorldNewsData.csv')
+        df['Date'] = pd.to_datetime(df['Date'], format='%d-%b-%y')
+
+        df_economic = self.read_economic_data()
+
+        # Find the earliest ENDING date between the two CSVs
+        min_end_date = min(df['Date'].max(), df_economic['Date'].max())
+
+        # Filter the dataframes to remove dates that go beyond the earliest ENDING date
+        df = df[df['Date'] <= min_end_date]
+        df_economic = df_economic[df_economic['Date'] <= min_end_date]
+
         output_path = f"data/Data_{ticker}.csv"
 
         if os.path.exists(output_path):
             # Read the existing DataFrame
             existing_df = pd.read_csv(output_path)
             last_date_existing = pd.to_datetime(existing_df['Date'].iloc[-1])
-            
+
             # Filter the main DataFrame to only process entries after the last_date_existing
-            df = df[pd.to_datetime(df['Date']) > last_date_existing]
+            df = df[df['Date'] > last_date_existing]
 
         # If there are new entries in df after filtering, compute sentiment scores
         if not df.empty:
-            df['Sentiment_Score'] = df.apply(lambda row: self.compute_score(row, ticker), axis=1)
-            
-            start_date = pd.to_datetime(df['Date'].iloc[0])
-            end_date = pd.to_datetime(df['Date'].iloc[-1]) + pd.Timedelta(days=1)
-            
+            df['Sentiment Score'] = df.apply(lambda row: self.compute_score(row, ticker), axis=1)
+
+            start_date = df['Date'].iloc[0]
+            end_date = df['Date'].iloc[-1] + pd.Timedelta(days=1)
+
             df_financial = self.fetch_financial_data(ticker, start_date, end_date)
 
             # Add technical indicators
             df_financial = self.add_technical_indicators(df_financial)
-            
+
             # Fetch fundamental data
             trailing_pe, forward_pe, dividend_yield = self.fetch_fundamental_data(ticker)
             df_financial['Trailing PE'] = [trailing_pe] * df_financial.shape[0]
             df_financial['Forward PE'] = [forward_pe] * df_financial.shape[0]
             df_financial['Dividend Yield'] = [dividend_yield] * df_financial.shape[0]
-            
-            # Merge sentiment data with financial data
-            df['Date'] = pd.to_datetime(df['Date'], format='%d-%b-%y')
-            df_combined = pd.merge(df, df_financial, left_on="Date", right_index=True, how="left")
-            
-            # Filter the combined dataframe to only include dates from the sentiment data
-            df_combined = df_combined[df_combined['Date'].between(start_date, end_date)]
-            
-            df_combined = self.handle_missing_data(df_combined)
-            
-            # Add the 'next day high' column
-            df_combined["Next_Day_High"] = df_combined["High"].shift(-1)
-            
-            columns_output = ['Date', 'Sentiment_Score', 'Open', 'High', 'Low', 'Close', 'Volume', 
-                            'RSI', 'MACD', 'MACD Signal', 'Upper Band', 'Middle Band', 'Lower Band', 
-                            'Trailing PE', 'Forward PE', 'Dividend Yield', 'Next_Day_High']
 
+            # Merge sentiment data with financial data and economic data
+            df_combined = pd.merge(df, df_financial, left_on="Date", right_index=True, how="left")
+            df_combined = pd.merge(df_combined, df_economic, on="Date", how="left")
+
+            # Handle missing data
+            df_combined = self.handle_missing_data(df_combined)
+
+            # Add the 'next day high' column
+            df_combined["Next Day High"] = df_combined["High"].shift(-1)
+
+            # Define the columns_output list
+            columns_output = [
+                'Date', 'Sentiment Score', 'Open', 'High', 'Low', 'Close', 'Volume', 
+                'RSI', 'MACD', 'MACD Signal', 'Upper Band', 'Middle Band', 'Lower Band',
+                'Trailing PE', 'Forward PE', 'Dividend Yield', 'GDP', 'UNRATE', 'T5YIE', 
+                'FEDFUNDS', 'CPIAUCSL', 'PPIACO', 'BOPGSTB', 'RSXFS', 'INDPRO', 'SP500', 
+                'EUR=X', 'XLB', 'XLC', 'XLE', 'XLF', 'XLI', 'XLK', 'XLP', 'XLRE', 'XLU', 
+                'XLV', 'XLY', 'Next Day High'
+            ]
             output_df = df_combined[columns_output].copy()
-                    
+
             # Convert 'Date' column to 'YYYY-MM-DD' format without time details
             output_df['Date'] = output_df['Date'].dt.strftime('%Y-%m-%d')
-                    
+
             # Set the 'Next_Day_High' for the last row to NaN
-            output_df.at[output_df.index[-1], 'Next_Day_High'] = float('NaN')
+            output_df.at[output_df.index[-1], 'Next Day High'] = float('NaN')
 
             # Append to existing or save new
             if os.path.exists(output_path):
                 existing_df = pd.read_csv(output_path)
-                final_df = pd.concat([existing_df, output_df], ignore_index=True)
-                final_df.to_csv(output_path, index=False)
+                combined_df = pd.concat([existing_df, output_df], ignore_index=True)
+                combined_df.to_csv(output_path, index=False)
             else:
-                if not os.path.exists("data"):
-                    os.makedirs("data")
                 output_df.to_csv(output_path, index=False)
-                
-            print("Data retrieved and saved successfully.")
-        else:
-            print(f"Data_{ticker}.csv is up-to-date.")
 
-sc = sentimentScorer()
-sc.score("GOOG")
+        else:
+            print(f"No new entries found for {ticker}. The data is up-to-date.")
+
+
+sc = dataGenerator()
+sc.score("LMT")
 
---
+++ econ.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import yfinance as yf
+import datetime
+from fredapi import Fred
+
+class EconomicDataUpdater:
+    def __init__(self, fred_api_key):
+        self.file_path = 'WorldEconomicData.csv'
+        self.fred = Fred(api_key=fred_api_key)
+        self.last_date = self.get_last_date()
+
+    def get_last_date(self):
+        df = pd.read_csv(self.file_path)
+        last_date_str = df['Date'].iloc[-1]
+        return datetime.datetime.strptime(last_date_str, '%d-%b-%y').date()
+
+    def get_all_days(self, start_date, end_date):
+        return [start_date + datetime.timedelta(days=i) for i in range((end_date-start_date).days + 1)]
+
+    def fetch_fred_data_range(self, start_date, end_date):
+        series_codes = ['GDP', 'UNRATE', 'T5YIE', 'FEDFUNDS', "CPIAUCSL", "PPIACO", "BOPGSTB", "RSXFS", "INDPRO", "SP500"]
+        data = {}
+        for series in series_codes:
+            series_data = self.fred.get_series(series, start_date, end_date)
+            series_data.index = series_data.index.date
+            data[series] = series_data
+        return data
+
+    def fetch_yfinance_data_range(self, start_date, end_date):
+        tickers = ["EUR=X", "XLK", "XLF", "XLV", "XLY", "XLP", "XLE", "XLI", "XLB", "XLRE", "XLU", "XLC"]
+        data = yf.download(tickers, start=start_date, end=end_date)
+        return data
+
+    def update_data(self):
+        today = datetime.date.today()
+        diff_days = (today - self.last_date).days
+        if diff_days > 1000:
+            today = self.last_date + datetime.timedelta(days=1000)
+
+        all_days = self.get_all_days(self.last_date, today)
+
+        yfinance_data = self.fetch_yfinance_data_range(self.last_date, today)
+        fred_data_range = self.fetch_fred_data_range(self.last_date, today)
+        
+        # Extract only the 'Close' values for each ticker
+        yfinance_data_closes = yfinance_data.xs('Close', axis=1, level=0)
+
+        new_rows = []
+        for date in all_days:
+            if date <= self.last_date:
+                continue
+
+            ts_date = pd.Timestamp(date)
+
+            # Get FRED data for the date. If not available, set as None
+            fred_data = {key: value[date] if date in value.index else None for key, value in fred_data_range.items()}
+
+            # Get yfinance data for the date. If not available, set as NaN (this is Pandas' default for missing data)
+            yf_row = yfinance_data_closes.loc[ts_date].to_dict() if ts_date in yfinance_data_closes.index else {ticker: float('nan') for ticker in yfinance_data_closes.columns}
+
+            row = {'Date': date.strftime('%d-%b-%y')}
+            row.update(fred_data)
+            row.update(yf_row)
+            new_rows.append(row)
+
+        df = pd.DataFrame(new_rows)
+
+        # Load the existing DataFrame
+        existing_df = pd.read_csv(self.file_path)
+
+        # Append the new rows to the existing DataFrame
+        combined_df = pd.concat([existing_df, df], ignore_index=True)
+
+        # Forward fill and back fill missing data
+        combined_df.fillna(method='ffill', inplace=True)  # forward fill
+        combined_df.fillna(method='bfill', inplace=True)  # backward fill after forward fill to ensure all NaNs are filled
+
+        columns_order = existing_df.columns.tolist()  # Get the existing column order
+
+        # Reorder the DataFrame columns to match the existing order
+        combined_df = combined_df[columns_order]
+
+        # Save the updated DataFrame back to the CSV
+        combined_df.to_csv(self.file_path, mode='w', header=True, index=False)
+
+
+
+
+
+
+
+# Usage
+updater = EconomicDataUpdater('1353cc07f1bd42904e62f96d8f984d86')
+updater.update_data()
